96faaa29052be24c284614783db8d801aa7fc85f
Недоруб Артем Александрович <anedorub@sfedu.ru>
2019-12-02 16:55:29 +0300

    new func LaterInYear

--- DateTimeMethods.pas
+++ DateTimeMethods.pas
@@ -20,7 +20,10 @@ begin
 //result:=(ABS(- p1) < ABS( -p2))?p1:p2;
 result:=((p1.Hour *3600 + p1.Minute *60 + p1.Second) > (p2.Hour *3600 + p2.Minute *60 + p2.Second))?p1:p2;
 end;
-function LaterInYear(p1, p2: DateTime): DateTime := p1;
+function LaterInYear(p1, p2: DateTime): DateTime;
+begin
+  result:=((p1.Date)>(p2.Date))?p1:p2;
+end;
 function DaysInYear(year: integer): integer := 0;
 function DaysInYearRange(year1, year2: integer): integer := 0;
 function SecondsInHours(hours: integer): integer := 0;
